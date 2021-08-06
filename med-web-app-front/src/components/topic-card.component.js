099eb36f2824b454dfd439d425c435cd886acc85
Ira Sheremet <irasheremet2002@gmail.com>
2021-08-06 16:34:19 +0300

    Change topic components

--- med-web-app-front/src/components/topic-card.component.js
+++ med-web-app-front/src/components/topic-card.component.js
@@ -1,8 +1,36 @@
 import React, {Component} from "react";
 import AuthService from "../services/auth.service";
 import '../styles/Record.css'
+import {Card, Grid, withStyles} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import {Link} from "react-router-dom";
 
-export default class TopicCard extends Component {
+
+const useStyles = theme => ({
+    mainGrid: {
+        display: 'flex',
+        margin:10,
+    },
+    grid: {
+        //margin: theme.spacing(1.5, 0, 0, 1),
+        display: 'flex',
+    },
+    paper: {
+        margin: theme.spacing(3),
+        borderRadius: 20,
+        backgroundColor: "#eeeeee"
+    },
+    hMargin: {
+        margin: 0
+    },
+    content: {
+        wordWrap: 'break-word',
+        maxWidth: 400,
+        marginLeft: 10,
+    },
+})
+
+class TopicCard extends Component {
     constructor(props) {
         super(props);
 
@@ -33,8 +61,10 @@ export default class TopicCard extends Component {
     }
 
     render() {
+        const {classes} = this.props;
         return (
-            <div className="row color-light-blue topic-card top-buffer-10">
+
+            /*<div className="row color-light-blue topic-card top-buffer-10">
                 <div className="col-sm ">
                     <div className="topic-info-box align-content-center">
                         <div className="center-vertical">
@@ -50,8 +80,44 @@ export default class TopicCard extends Component {
                         <div className="bottom-buffer-10"> {this.topic.name}</div>
                     </header>
                 </div>
-            </div>
+            </div>*/
+            <Grid spacing={1}>
+                <Card className={classes.paper}>
+                    <Grid className={classes.mainGrid}>
+                        <Grid className={classes.grid}>
+                            <Grid className={classes.grid}>
+                                <Link to={"/profile/" + this.topic.creator.username}
+                                      style={{textDecoration: 'none', color: 'dark-blue'}}>
+                                    <h6 className={classes.hMargin}> {this.topic.creator.username}</h6>
+                                </Link>
+
+                            </Grid>
+                            {/*<Grid item>
+                            <Typography variant={"subtitle1"}>
+                                {new Date(this.topic.creationTime).toLocaleDateString()}
+                            </Typography>
+
+                        </Grid>
+                        <Grid item>
+                            <Typography variant={"subtitle1"}>
+                                {this.creationTime}
+                            </Typography>
+                        </Grid>*/}
+                            {/*</Grid>*/}
+                            <Grid>
+                                <Typography variant="body1" className={classes.content}>{/*gutterBottom*/}
+                                    {this.topic.name}
+                                </Typography>
+
+                            </Grid>
+                        </Grid>
+
+                    </Grid>
+                </Card>
+            </Grid>
         )
 
     }
 }
+
+export default withStyles(useStyles)(TopicCard)
